0d4bf987722110a9875177c3c5727b98d5e7c49b
korovkinada <korovkinada@ya.ru>
2017-07-18 02:06:20 +0800

    v1.2

--- loader.php
+++ loader.php
@@ -42,5 +42,15 @@ function getPhp($dir) {
     return $phpfiles;
 }
 
+class MyException extends Exception
+{
+    public function __construct($message, $code = 0) {
+        parent::__construct($message, $code);
+    }
+
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
 ?>
 
